5eb51e3c4f7316689c2c20cffcaf8655ea392202
QUA209.003 <QUA209.003@LAB-F05-12.academico.local>
2019-07-02 19:59:08 -0300

    Primeiro UP

---
+++ ControleManutencao/WebContent/quadro.jsp
@@ -0,0 +1,75 @@
+<%@page import="org.senai.contrManutencao.modelo.Quadro"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<title>Controle de Manutenção</title>
+</head>
+<body>
+<div class="container">
+<div align="center"><img src="img/allia.jpg"
+	height="240" width="400"/>
+	<div align="center"/>
+	<a href="gerenciarQuadro.jsp">Abertura de Chamado</a>
+	
+
+	
+	<h1>Controle de Serviço</h1>
+</div>
+            
+  
+  <table class="table table-striped">
+  
+    <thead>
+      <tr>
+     
+        <th width="25%">Descrição do Serviço<p></th>
+		<th width="17%">Status<p></th>
+	    <th width="17%">Local<p></th> 
+		<th width="17%">Responsavel<p></th>
+		<th width="15%">Data Início<p></th>
+		<th width="15%">Data Fim<p></th>
+		
+      </tr>
+    </thead>
+    <tbody>
+      <script>
+      
+      function prepararEditar(cod){
+      //alert("ediatar "+cod);
+     window.location.replace('gerenciarQuadro.jsp?cod='+cod);
+      }
+      
+      </script>
+      
+      <%
+      Quadro quadro = new Quadro();
+      
+      for (Quadro p : quadro.getLista()) {
+    	out.print("<tr onclick='prepararEditar("+p.getCod()+")'>");
+    	//out.print("<td>"+p.getCod()+"</td>");
+    	out.print("<td>"+ p.getDescricao()+"</td>");
+    	out.print("<td class='"+p.getStatus()+"'>" + p.getStatus()+"</td>");
+    	out.print("<td>"+ p.getLocal()+"</td>");
+    	out.print("<td>"+ p.getObservacao()+"</td>");
+    	out.print("<td>"+ p.getResponsavel()+"</td>");
+    	out.print("<td>"+ p.getDtInicio()+"</td>");
+    	out.print("<td>"+ p.getDtFim()+"</td>");
+    	out.print("</tr>");
+    	
+      } 
+      
+      %>
+    </tbody>
+  </table>
+
+</div>
+</body>
+</html>
---
+++ ControleManutencao/src/org/senai/contrManutencao/modelo/Quadro.java
@@ -0,0 +1,310 @@
+package org.senai.contrManutencao.modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.senai.contrManutencao.modelo.ConectarJDBC;
+
+public class Quadro {
+
+	
+	private int cod;
+	private String descricao;
+	private String status;
+	private String local;
+	private String responsavel;
+	private String dtInicio;
+	private String dtFim;
+	private String observ;
+	
+	
+	
+	public Quadro(){
+		setCod(0);
+		setDescricao("");
+		setStatus("");
+		setLocal("");
+		setObservacao("");
+		setResponsavel("");
+		setDtInicio("");
+		setDtFim("");
+	
+	}
+
+
+	public int getCod() {
+		return cod;
+	}
+
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getObservacao() {
+		return observ;
+	}
+
+
+	public void setObservacao(String observ) {
+		this.observ = observ;
+	}
+	
+	
+	
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+
+	public String getDtInicio() {
+		return dtInicio;
+	}
+
+
+	public void setDtInicio(String dtInicio) {
+		this.dtInicio = dtInicio;
+	}
+
+
+	public String getDtFim() {
+		return dtFim;
+	}
+
+
+	public void setDtFim(String dtFim) {
+		this.dtFim = dtFim;
+	}
+
+	public boolean apagar() {
+
+		Connection conexao = new ConectarJDBC().getConectar();
+		
+		if(conexao != null) {
+			String sql = "delete from quadro where cod = ? ";
+			try {
+				PreparedStatement prepararSQL =
+						conexao.prepareStatement(sql);
+				prepararSQL.setInt(1, cod);
+				
+				prepararSQL.execute();
+				prepararSQL.close();
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+			
+		}
+		return false;
+	}
+	
+		//copiamos o codigo  inserir para reaproveitar o codigo e 
+		//fazer o metodo atualizar
+	public boolean atualizar() {
+
+		Connection conexao = new ConectarJDBC().getConectar();
+		
+		if(conexao != null) {
+			String sql = "update painel set " + 
+					"	descricao = ? ," + 
+					"	status        = ? ," + 
+					"	local        = ? ," + 
+					"	observ        = ? ," + 
+					"	responsavel  = ? ," + 
+					"	dt_inicio  = ? ," + 
+					"	dt_fim   = ?," + 
+					" where cod = ? ";
+			try {
+				PreparedStatement prepararSQL =
+						conexao.prepareStatement(sql);
+				prepararSQL.setString(1, descricao);
+				prepararSQL.setString(2, status);
+				prepararSQL.setString(3, local);
+				prepararSQL.setString(4, observ);
+				prepararSQL.setString(5, responsavel);
+				prepararSQL.setString(6, dtInicio);
+				prepararSQL.setString(7, dtFim);
+				prepararSQL.setInt(8, cod);
+				
+				prepararSQL.execute();
+				prepararSQL.close();
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+			
+		}
+		return false;
+	}
+	
+	public boolean inserir() {
+
+		Connection conexao = new ConectarJDBC().getConectar();
+		
+		if(conexao != null) {
+			String sql = "insert into quadro(" + 
+					"	descricao  ," + 
+					"	status      ," + 
+					"	local       ," + 
+					"	observ        ," + 
+					"	responsavel   ," + 
+					"	dt_inicio   ," + 
+					"	dt_fim   " + 
+					"	 )" + 
+					"values (?," + 
+					"		?," + 
+					"		?," + 
+					"		?," +
+					"		?," +
+					"		?," + 
+					"		? " + 
+					"		)";
+			try {
+				PreparedStatement prepararSQL =
+						conexao.prepareStatement(sql);
+				prepararSQL.setString(1, descricao);
+				prepararSQL.setString(2, status);
+				prepararSQL.setString(3, local);
+				prepararSQL.setString(4, observ);
+				prepararSQL.setString(5, responsavel);
+				prepararSQL.setString(6, dtInicio);
+				prepararSQL.setString(7, dtFim);
+				prepararSQL.execute();
+				prepararSQL.close();
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+			
+		}
+		return false;
+	}
+	
+	public List<Quadro> getLista(){		
+		try {
+			Connection conexao = new ConectarJDBC().getConectar();
+			PreparedStatement ps = 					
+					conexao.prepareStatement("select * from quadro");
+			ResultSet rs = ps.executeQuery();
+			
+			List<Quadro> lsQuadro = new ArrayList<Quadro>();
+			
+			while (rs.next()) {
+				Quadro p = new Quadro();
+				// o que esta entre aspas "nome_da_coluna_no_banco"
+				p.setCod(rs.getInt("cod"));
+				p.setDescricao(rs.getString("descricao"));
+				p.setStatus(rs.getString("status"));
+				p.setLocal(rs.getString("local"));
+				p.setLocal(rs.getString("observ"));
+				p.setResponsavel(rs.getString("responsavel"));
+				p.setDtInicio(rs.getString("dt_inicio"));
+				p.setDtFim(rs.getString("dt_fim"));
+				lsQuadro.add(p);
+			}
+			ps.close();
+			conexao.close();
+			return lsQuadro;
+		} catch (Exception e) {
+
+		}		
+		return null;
+	}
+	
+	public Quadro getQuadro(int cod){		
+		try {
+			Connection conexao = new ConectarJDBC().getConectar();
+			PreparedStatement ps = 					
+					conexao.prepareStatement("select * from quadro where cod = ? ");
+			ps.setInt(1, cod);
+			ResultSet rs = ps.executeQuery();
+			
+			Quadro p = new Quadro();
+			
+			while (rs.next()) {
+				
+				// o que esta entre aspas "nome_da_coluna_no_banco"
+				p.setCod(rs.getInt("cod"));
+				p.setDescricao(rs.getString("descricao"));
+				p.setStatus(rs.getString("status"));
+				p.setLocal(rs.getString("local"));
+				p.setLocal(rs.getString("observ"));
+				p.setResponsavel(rs.getString("responsavel"));
+				p.setDtInicio(rs.getString("dt_inicio"));
+				p.setDtFim(rs.getString("dt_fim"));
+		
+			}
+			ps.close();
+			conexao.close();
+			return p;
+		} catch (Exception e) {
+
+		}		
+		return null;
+	}
+	
+	public String getCorStatus() {
+			
+		if(status.equals("Agendado")) {
+			return "table-warning";
+		}
+
+		if(status.equals("Em andamento")) {
+			return "table-danger";
+		}		
+		
+		if(status.equals("Parado")) {
+			return "table-success";
+		}
+
+		if(status.equals("Concluído")) {
+			return "table-primary";
+		}
+		
+		return "";
+	}
+
+
+}
+
